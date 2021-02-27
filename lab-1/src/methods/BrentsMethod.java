c91a36838aeec624daf1beb73aeaff119800c9ed
Brat-vseznamus <fvixnin@gmail.com>
2021-02-27 20:03:57 +0300

    add classes

---
+++ lab-1/src/methods/BrentsMethod.java
@@ -0,0 +1,18 @@
+package methods;
+
+import java.util.function.Function;
+
+public class BrentsMethod extends AbstractMethod {
+
+    public BrentsMethod(Function<Double, Double> function) {
+        super(function);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public double findMin(double a, double b) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
---
+++ lab-1/src/methods/GoldenRationMethod.java
@@ -0,0 +1,18 @@
+package methods;
+
+import java.util.function.Function;
+
+public class GoldenRationMethod extends AbstractMethod {
+
+    public GoldenRationMethod(Function<Double, Double> function) {
+        super(function);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public double findMin(double a, double b) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
