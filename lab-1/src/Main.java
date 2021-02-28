39af1b7811355faf7027ee230032150c787d2a3d
Brat-vseznamus <fvixnin@gmail.com>
2021-02-28 04:25:52 +0300

    add fibonacci but it's not working

--- lab-1/src/Main.java
+++ lab-1/src/Main.java
@@ -1,5 +1,6 @@
 import methods.BrentsMethod;
 import methods.DichotomyMethod;
+import methods.FibonacciMethod;
 import methods.GoldenRatioMethod;
 import methods.OptimizationAlgorithm;
 
@@ -9,12 +10,17 @@ public class Main {
     public static void main(String[] args) {
         UnaryOperator<Double> function;
         function = x -> x * x + Math.exp(-0.35d * x);
-
+        // function = x -> x * Math.sin(x) + 2 * Math.cos(x);
+        double left = -2d;
+        double right = 3d;
+        
         OptimizationAlgorithm method = new DichotomyMethod(function);
         OptimizationAlgorithm method2 = new GoldenRatioMethod(function);
-        System.out.println();
-        System.out.println("Result of dichotomy: " + method.findMin(-2d, 3d));
-        System.out.println("Result of golden ratio: " + method2.findMin(-2d, 3d));
+        OptimizationAlgorithm method3 = new FibonacciMethod(function);
         
+        System.out.println();
+        System.out.println("Result of dichotomy: " + method.findMin(left, right));
+        System.out.println("Result of golden ratio: " + method2.findMin(left, right));
+        System.out.println("Result of fibonacci: " + method3.findMin(left, right));
     }
 }
--- lab-1/src/methods/FibonacciMethod.java
+++ lab-1/src/methods/FibonacciMethod.java
@@ -1,8 +1,11 @@
 package methods;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.UnaryOperator;
 
 public class FibonacciMethod extends AbstractMethod {
+    List<Long> fs;
 
     public FibonacciMethod(UnaryOperator<Double> function) {
         super(function);
@@ -14,8 +17,64 @@ public class FibonacciMethod extends AbstractMethod {
 
     @Override
     public double findMin(double a, double b) {
-        // TODO Auto-generated method stub
-        return 0;
+        fs = new ArrayList<>();
+        fs.add(0l);
+        fs.add(1l);
+        long lastFibonacci = (long)((b - a) / eps);
+        // System.out.println(lastFibonacci);
+        while (lastFibonacci > getF(0)) {
+            long tmp = fs.get(fs.size() - 1);
+            tmp += fs.get(fs.size() - 2);
+            fs.add(tmp);
+        }
+        double lamda = a + getF(-2) * (b - a) / (double)getF(0);
+        double mu = a + getF(-1) * (b - a) / (double)getF(0);
+        int k = 1;
+        double fx1 = function.apply(lamda);
+        double fx2 = function.apply(mu);
+        double an = a, bn = b;
+        while (k < fs.size()) {
+            // step 1
+            if (fx1 > fx2) {
+                // step 2
+                an = lamda;
+                lamda = mu;
+                mu = an + getF(-k-1) * (bn - an) / (double)getF(-k);
+                if (k == fs.size() - 2) {
+                    // go to step 5
+                    break;
+                } else {
+                    fx2 = function.apply(mu);
+                }
+            } else {
+                // step 3
+                bn = mu;
+                mu = lamda;
+                lamda = an + getF(-k-2) * (bn - an) / (double)getF(-k);
+                if (k == fs.size() - 2) {
+                    // go to step 5
+                    break;
+                } else {
+                    fx1 = function.apply(lamda);
+                }
+            }
+            // step 4
+            k++;
+            System.out.println(String.format("[%s, %s]", an, bn));
+        }
+        // step 5
+        mu = lamda + eps;
+        if (function.apply(lamda).equals(function.apply(mu))) {
+            an = lamda;
+        } else {
+            bn = mu;
+        }
+        return (an + bn) / 2d;
+    }
+
+
+    public Long getF(int k) {
+        return fs.get(fs.size() + k - 1);
     }
 
     
