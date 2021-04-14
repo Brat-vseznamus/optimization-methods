f6cf2a7c079e8061c930ee06a275084886ed5180
Brat-vseznamus <fvixnin@gmail.com>
2021-04-14 06:16:50 +0300

    aeee

--- lab-2/src/main/java/functions/AbstractMethod.java
+++
@@ -1,9 +0,0 @@
-package functions;
-
-public abstract class AbstractMethod {
-    private QuadraticForm form;
-
-    protected AbstractMethod(QuadraticForm form) {
-        this.form = form;
-    }
-}
---
+++ lab-2/src/main/java/functions/SteepestDescendMethod.java
@@ -0,0 +1,50 @@
+package functions;
+
+import java.util.Arrays;
+
+public class SteepestDescendMethod extends AbstractGradientMethod {
+
+    public SteepestDescendMethod(QuadraticForm form, double eps) {
+        super(form, eps);
+    }
+
+    public SteepestDescendMethod(QuadraticForm form) {
+        super(form);
+    }
+
+    public double[] findMin() {
+        int n = form.getN();
+        // step 1
+        double[] x = new double[n];
+        double[] y = new double[n];
+        double f_x = form.apply(x);
+        double f_y;
+
+        while (true) {
+            // step 2
+            double[] gradient = form.gradient(x);
+            double norm = Arrays.stream(gradient).sum();
+            if (norm < eps) {
+                break;
+            }
+
+            // step 3
+            double a = 0d, b = 2d / form.getMaxValue();
+            // UnaryOperator<Double> function = (arg) -> {
+                // double[] vector = new double[n];
+                // for (int i = 0; i < n; ++i) {
+                    // vector[i] = vector[i] - arg * gradient[i];
+                // }
+                // return f.apply(vector);
+            // };
+            // OptimizationAlgorithm brent = new BrentsMethod(function);
+            // double alpha = brent.findMin(a, b);
+            
+            // for (int i = 0; i < n; ++i) {
+                // x[i] = x[i] - alpha * gradient[i];
+            // }
+        }
+
+        return x;
+    }
+}
