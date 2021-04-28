016192bae64d5b30c40261094695922d22cc5f61
Tycoon <ntpmosakk070601@gmail.com>
2021-04-28 03:43:33 +0300

    small changes in Main

--- src/main/java/functions/Main.java
+++ src/main/java/functions/Main.java
@@ -4,48 +4,26 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(final String[] args) {
-        // double[][] a = {
-        //     {1, 0},
-        //     {0, 3}
-        // };
-        // double[] b = {2, 3};
-        // double c = 4;
-        // double[] values = {1, 3};
-        // Matrix a = new Matrix(
-        //     new DoubleVector(1d, 0d),
-        //     new DoubleVector(0d, 3d)
-        // );
-        // DoubleVector b = new DoubleVector(2d, 3d);
-        // Double c = 4d;
-        // DoubleVector values = new DoubleVector(1d, 3d);
-        // QuadraticForm form = new QuadraticForm(a, b, c, values);
         int dim = 4;
         int mu = 3;
-        if (args.length == 2) {
+        if (args.length != 0 && args.length != 2) {
             try {
                 dim = Integer.parseInt(args[0]);
                 mu = Integer.parseInt(args[1]);
             } catch (final NumberFormatException e) {
-                System.err.println("Two integers wsa expected: " + e.getMessage());
+                System.out.println("Usage:");
+                System.out.println("\t\tjava function.Main <dimensions> <mu>");
+                System.out.println("\t\tjava function.Main");
                 return;
             }
         }
+      
         final QuadraticForm form = FormGenerator.generate(dim, mu);
-        // System.out.println(form.getA());
-        // System.out.println(form.getB());       
+      
         final OptimisationGradientMethod method = new GradientDescendMethod(form);
         final OptimisationGradientMethod method2 = new SteepestDescendMethod(form);
         final OptimisationGradientMethod method3 = new ConjugateGradientMethod(form);
-        // DoubleVector x = new DoubleVector(1d, 1d, 1d);
-        // System.out.println(form.apply(x));
-        // System.out.println(form.gradient(x));
-        // DoubleVector v = new DoubleVector(1d, 2.4, 3d);
-        // Matrix mat = new Matrix(
-        //     new DoubleVector(2d, 1d, 1d), 
-        //     new DoubleVector(2d, 1d, 1d),
-        //     new DoubleVector(1d, 1d, 2d));
-        // System.out.println(v.multiply(mat));
-        // System.out.println(mat.multiply(v));
+      
         final long start = System.currentTimeMillis();
         final DoubleVector res1 = new DoubleVector(method.findMin());
         System.out.println(res1);
