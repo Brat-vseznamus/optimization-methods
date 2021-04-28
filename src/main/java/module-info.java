4fa29a1440eb0a384dac40fdb2bb8425fd30c0b0
salavay <284544@niuitmo.ru>
2021-04-28 17:18:21 +0300

    Fixed module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module grapics {
     requires javafx.controls;
     requires javafx.fxml;
     requires jfxutils;
+    requires com.jfoenix;
 
     opens grapics to javafx.fxml;
     exports grapics;
