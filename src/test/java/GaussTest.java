231fb1abfc172c5d0213963be5a8ffcb72e9f3f1
salavay <284544@niuitmo.ru>
2021-05-19 01:22:53 +0300

    refactored tests

--- src/test/java/GaussTest.java
+++ src/test/java/GaussTest.java
@@ -4,11 +4,13 @@ import slau.matrix.Matrix;
 import slau.matrix.RegularMatrix;
 import slau.methods.GaussMethod;
 
-import java.util.Arrays;
+import java.util.stream.IntStream;
 
 import static java.lang.Double.NaN;
 
 public class GaussTest {
+
+    final static double DOUBLE_EQUALS_EPS = 0.0001;
 
     @Test
     public void gaussD3Test() {
@@ -20,8 +22,7 @@ public class GaussTest {
                 });
         final double[] numbers = new double[]{4, 5, 6};
         final double[] answer = {1d, -3d, 3d};
-        Assertions.assertTrue(Arrays.equals(answer,
-                new GaussMethod().solve(m, numbers)));
+        assertEqualsDoubleArrays(answer, new GaussMethod().solve(m, numbers));
     }
 
     @Test
@@ -35,7 +36,12 @@ public class GaussTest {
                 });
         final double[] numbers = new double[]{1, 8, 2, 3};
         final double[] answer = {NaN, NaN, NaN, NaN};
-        Assertions.assertTrue(Arrays.equals(answer,
-                new GaussMethod().solve(m, numbers)));
+        assertEqualsDoubleArrays(answer, new GaussMethod().solve(m, numbers));
+    }
+
+    private void assertEqualsDoubleArrays(final double[] first,
+                                          final double[] second) {
+        Assertions.assertEquals(first.length, second.length);
+        IntStream.range(0, first.length).forEach(index -> Assertions.assertEquals(first[index], second[index], DOUBLE_EQUALS_EPS));
     }
 }
