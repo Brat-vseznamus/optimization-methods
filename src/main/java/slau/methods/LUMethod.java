011787eac20ba57aa0ddcbe7eb267cd80232c82e
Tycoon <ntpmosakk070601@gmail.com>
2021-05-20 00:59:56 +0300

    code clean up (3)

--- src/main/java/slau/methods/LUMethod.java
+++ src/main/java/slau/methods/LUMethod.java
@@ -2,18 +2,16 @@ package slau.methods;
 
 import slau.matrix.LU;
 import slau.matrix.LUDecomposible;
-import slau.matrix.LUMatrix;
 import slau.matrix.Matrix;
 
 public class LUMethod implements Method{
     @Override
     public double[] solve(final Matrix matrix, final double[] numbers) {
-//        if (!(matrix instanceof LUMatrix)) {
-//            throw new IllegalArgumentException("Matrix must have LU decomposition.");
-//        }
+        if (!(matrix instanceof LUDecomposible)) {
+            throw new IllegalArgumentException("Matrix must have LU decomposition.");
+        }
         final LU lu = ((LUDecomposible) matrix).getLU();
         final Method gauss = new GaussMethod();
-//        double[] y = gauss.solve(lu.getL(), numbers);
         return gauss.solve(lu.getU(),
                 gauss.solve(lu.getL(), numbers));
     }
