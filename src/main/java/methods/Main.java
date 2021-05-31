8ebe13341ce0d3580f7850bcdec7adf673921cc6
Tycoon <ntpmosakk070601@gmail.com>
2021-06-01 02:18:00 +0300

    added Sparse Matrix

--- src/main/java/methods/Main.java
+++ src/main/java/methods/Main.java
@@ -3,40 +3,32 @@ package methods;
 import methods.dimensional.one.*;
 import methods.dimensional.poly.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static DoubleVector answer;
 
-    private static final List<QuadraticForm> forms = List.of(
+    private static final List<QuadraticForm> forms = new ArrayList<>(List.of(
             new QuadraticForm(
                     new DiagonalMatrix(new DoubleVector(60d, 2d)),
                     new DoubleVector(-10d, 10d), 2d),
-
             new QuadraticForm(
-                    new DiagonalMatrix(new DoubleVector(1d, 64d)),
-                    new DoubleVector(-10, 30),
-                    2d),
-
+                    new DiagonalMatrix(new DoubleVector(0.5d, 32d)),
+                    new DoubleVector(-5d, 15d), 2d),
             new QuadraticForm(
-                    new DiagonalMatrix(new DoubleVector(0.5d, 0.5d)),
-                    new DoubleVector(40d, -2d), 13d),
-
-            new QuadraticForm(
-                    new DiagonalMatrix(new DoubleVector(40d, 2d)),
-                    new DoubleVector(-100d, 345d), -5d),
-
-            new QuadraticForm(
-                    new DiagonalMatrix(new DoubleVector(254 * 2d, 254 * 2d)),
-                    new DoubleVector(50d, 130d), -111d),
-
+                    new DiagonalMatrix(new DoubleVector(51.3d * 2, 27.9d * 2)),
+                    new DoubleVector(-23.78d, -0.9d), -0.78d),
             new QuadraticForm(
                     new Matrix(new DoubleVector(254 * 2d, 506 / 2d),
                             new DoubleVector(506 / 2d, 254 * 2d)),
                     new DoubleVector(50d, 130d), -111d,
-                    new DoubleVector(508, 508))
-    );
+                    new DoubleVector(508, 508)),
+            new QuadraticForm(
+                    new DiagonalMatrix(new DoubleVector(254 * 2d, 254 * 2d)),
+                    new DoubleVector(50d, 130d), -111d)
+    ));
 
     public static void main(final String[] args) {
         int dim = 500;
@@ -68,49 +60,13 @@ public class Main {
             System.out.println(steepest.getTable().size());
         }
         if (mode == 1) {
-
             answer = measureMethod(conjugate);
             measureMethod(steepest);
-            // System.out.println(form.getA());
-            // System.out.println(form.getB()); 
-            // final DoubleVector v = new DoubleVector(1d, 1d, 1d);
-            // // System.out.println(form.getA().multiply(v));
-            // // System.out.println(v.multiply(form.getA()));
-            // // System.out.println(form.gradient(v));
-            // // System.out.println(form.apply(v));
-            // final long start = System.currentTimeMillis();
-            // // final DoubleVector res1 = new DoubleVector(gradient.findMin());
-            // // // System.out.println(res1);
-            // final String timeFormat = "Time for %s: %f sec.%n";
-            // answer = new DoubleVector(gradient.findMin());
-            // // final DoubleVector gradRes = new DoubleVector(answer);
-            // final long end0 = System.currentTimeMillis();
-            // System.out.printf(timeFormat, "GradientDescendMethod", (end0 - start) / 1000.0);
-            // // System.out.printf("iteration number: %d%n", gradient.getTable().size());
-
-            // final DoubleVector steepRes = new DoubleVector(steepest.findMin());
-            // final long end1 = System.currentTimeMillis();
-            // System.out.printf(timeFormat, "SteepestDescendMethod", (end1 - end0) / 1000.0);
-            // System.out.printf("iteration number: %d%n", steepest.getTable().size());
-            // System.out.println("steepRes = " + steepRes);
-            // System.out.println();
-
-            // final DoubleVector res3 = new DoubleVector(conjugate.findMin());
-            // answer = res3;
-            // // System.out.printf(timeFormat, "ConjugateGradientMethod", (end1 - end0) / 1000.0);
-            // // System.out.printf("iteration number: %d%n", conjugate.getTable().size());
-
-            // final DoubleVector res2 = new DoubleVector(steepest.findMin());
-            // final long end2 = System.currentTimeMillis();
-            // System.out.printf(timeFormat, "SteepestDescendMethod", (end2 - end1) / 1000.0);
-            // // System.out.printf("iteration number: %d%n", steepest.getTable().size());
-
-            // System.out.printf("Time duration: %f sec.%n", (System.currentTimeMillis() - start) / 1000.0);
         } else if (mode == 2) {
-            // System.out.println("Gradient:");
-            // outputMethodInfo(gradient);
-//            System.out.println("Conjugate:");
-//            outputMethodInfo(conjugate);
+             System.out.println("Gradient:");
+             outputMethodInfo(gradient);
+            System.out.println("Conjugate:");
+            outputMethodInfo(conjugate);
             System.out.println("Steepest:");
             outputMethodInfo(steepest);
         } else if (mode == 3) {
@@ -135,19 +91,12 @@ public class Main {
 
     private static void outputMethodInfo(final GradientOptimizationMethod method) {
         final List<Pair<Integer, List<Pair<Integer, Integer>>>> methodInfo = method.valueAndDimToIterations();
-//        for (final Pair<Integer, List<Pair<Integer, Integer>>> row : methodInfo) {
-//            System.out.printf("%d ", row.first);
-//            for (final Pair<Integer, Integer> cell : row.second) {
-//                System.out.printf("%d ", cell.second);
-//            }
-//            System.out.println();
-//        }
     }
 
     public static DoubleVector measureMethod(final GradientOptimizationMethod method) {
-        long start = System.currentTimeMillis();
-        DoubleVector res = new DoubleVector(method.findMin());
-        long end = System.currentTimeMillis();
+        final long start = System.currentTimeMillis();
+        final DoubleVector res = new DoubleVector(method.findMin());
+        final long end = System.currentTimeMillis();
         System.out.printf("Method %s found minimum for time: %fs%n", method.getClass().getSimpleName(), (end - start) / 1000d);
         return res;
     }
---
+++ src/test/java/SparseMatrixTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import slau.matrix.Matrix;
+import slau.matrix.SymmetricSparseMatrix;
+
+import java.util.stream.IntStream;
+
+public class SparseMatrixTest {
+
+    @Test
+    public void sparseMatrixGet() {
+        final double[][] data = {
+                {1, 2, 0, 2, 0},
+                {2, 4, 6, 0, 5},
+                {0, 6, 5, 0, 6},
+                {2, 0, 0, 9, 0},
+                {0, 5, 6, 0, 4}};
+        final Matrix sparseMatrix = new SymmetricSparseMatrix(data);
+
+        System.out.println(sparseMatrix.output());
+
+        final boolean result = IntStream.range(0, 5).allMatch(i ->
+                IntStream.range(0, 5).allMatch(j -> {
+                    final boolean eq = data[i][j] == sparseMatrix.get(i, j);
+                    if (!eq) {
+                        System.out.printf("data[%d][%d] = %f and matrix.get(%d, %d), = %f %n",
+                                i, j, data[i][j], i, j, sparseMatrix.get(i, j));
+                    }
+                    return eq;
+                })
+        );
+
+
+
+        Assertions.assertTrue(result);
+    }
+}
