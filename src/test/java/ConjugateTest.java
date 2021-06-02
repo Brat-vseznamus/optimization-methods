9d2a4270df91ded955ffeacab20210d1192a8b18
Tycoon <ntpmosakk070601@gmail.com>
2021-06-03 01:41:29 +0300

    добавлен нерабочий метод conjugate + tests :^)

---
+++ src/test/java/ConjugateTest.java
@@ -0,0 +1,77 @@
+import org.junit.jupiter.api.Test;
+import slau.matrix.Matrix;
+import slau.matrix.RegularMatrix;
+import slau.methods.ConjugateGradientMethod;
+
+import java.util.Arrays;
+
+public class ConjugateTest {
+
+    @Test
+    public void conjugateEasyTest() {
+        final Matrix m = new RegularMatrix(
+                new double[][]{
+                        {1, 0, 0, 0, 0, 0},
+                        {0, 1, 0, 0, 0, 0},
+                        {0, 0, 1, 0, 0, 0},
+                        {0, 0, 0, 1, 0, 0},
+                        {0, 0, 0, 0, 1, 0},
+                        {0, 0, 0, 0, 0, 1}
+                });
+        final double[] numbers = {1, 1, 1, 1, 1, 1};
+        final double[] answer = {1, 1, 1, 1, 1, 1};
+        final double[] solve = new ConjugateGradientMethod().solve(m, numbers);
+        TestUtils.assertEqualsDoubleVectorNorms(answer, solve);
+    }
+
+    @Test
+    public void conjugateEasy2Test() {
+        final Matrix m = new RegularMatrix(
+                new double[][]{
+                        {1, 0, 0, 0, 0, 0},
+                        {0, 5, 0, 0, 0, 0},
+                        {0, 0, -10, 0, 0, 0},
+                        {0, 0, 0, 8, 0, 0},
+                        {0, 0, 0, 0, 2, 0},
+                        {0, 0, 0, 0, 0, 9}
+                });
+        final double[] numbers = {1, 5, -10, 8, 2, 9};
+        final double[] answer = {1, 1, 1, 1, 1, 1};
+        final double[] solve = new ConjugateGradientMethod().solve(m, numbers);
+        System.out.println(Arrays.toString(solve));
+        TestUtils.assertEqualsDoubleVectorNorms(answer, solve);
+    }
+
+    @Test
+    public void conjugateEasy3Test() {
+        final Matrix m = new RegularMatrix(
+                new double[][]{
+                        {1, 0, 0, 0, 0, 0},
+                        {0, 5, 0, 0, 0, 0},
+                        {0, 0, -10, 0, 0, 0},
+                        {0, 0, 0, 8, 0, 0},
+                        {0, 0, 0, 0, 2, 0},
+                        {0, 0, 0, 0, 0, 9}
+                });
+        final double[] answer = {3, -1, -10, 17, 18, -20.5};
+        final double[] numbers = {3, -5, 100, 136, 36, -184};
+        final double[] solve = new ConjugateGradientMethod().solve(m, numbers);
+        System.out.println(Arrays.toString(solve));
+        TestUtils.assertEqualsDoubleVectorNorms(answer, solve);
+    }
+
+    @Test
+    public void conjugateD3Test() {
+        final Matrix m = new RegularMatrix(
+                new double[][]{
+                        {1, 2, -30},
+                        {2, 5, 4},
+                        {3, 5, 6}
+                });
+        final double[] answer = {1d, -3d, 3d};
+        final double[] numbers = new double[]{-95d, -1d, 6d};
+        final double[] solve = new ConjugateGradientMethod().solve(m, numbers);
+        TestUtils.assertEqualsDoubleArrays(answer, solve);
+    }
+
+}
--- src/test/java/TestUtils.java
+++ src/test/java/TestUtils.java
@@ -1,14 +1,19 @@
+import methods.dimensional.poly.DoubleVector;
 import org.junit.jupiter.api.Assertions;
 
 import java.util.stream.IntStream;
 
 public abstract class TestUtils {
 
-    final static double DOUBLE_EQUALS_EPS = 0.0001;
+    final static double DOUBLE_EQUALS_EPS = 0.001;
 
     static void assertEqualsDoubleArrays(final double[] first,
                                          final double[] second) {
         Assertions.assertEquals(first.length, second.length);
         IntStream.range(0, first.length).forEach(index -> Assertions.assertEquals(first[index], second[index], DOUBLE_EQUALS_EPS));
+    }
+
+    static void assertEqualsDoubleVectorNorms(final double[] first, final double[] second) {
+        Assertions.assertEquals(new DoubleVector(first).subtract(new DoubleVector(second.length)).norm(), DOUBLE_EQUALS_EPS);
     }
 }
