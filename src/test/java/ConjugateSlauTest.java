4fddf0128f0c315ca970bd41569dc589e209f1b7
Tycoon <ntpmosakk070601@gmail.com>
2021-06-08 03:33:04 +0300

    conjugate slau tests

--- src/test/java/ConjugateSlauTest.java
+++ src/test/java/ConjugateSlauTest.java
@@ -1,27 +1,56 @@
 import linear.DoubleVector;
 import linear.Matrix;
-import methods.Pair;
 import org.junit.jupiter.api.Test;
 import slau.methods.ConjugateGradientMethod;
-import slau.methods.Method;
 import slau.utils.FormulaGenerator;
 
+import java.util.function.Function;
 import java.util.stream.IntStream;
 
 public class ConjugateSlauTest {
 
-    @Test
-    public void hilbertTest() {
+    private DoubleVector rangeVector(final int n) {
+        final DoubleVector x = new DoubleVector(n);
+        IntStream.range(0, n).forEach(i -> x.set(i, i + 1d));
+        return x;
+    }
+
+    private void test(final Function<Integer, Matrix> generator, final int n1, final int n2) {
         final ConjugateGradientMethod conjugate = new ConjugateGradientMethod();
-        for (int n = 10; n <= 1000; n = (int) (((double) n) * Math.E)) {
-            final Matrix matrix = FormulaGenerator.generateHilbert(n);
-            final DoubleVector x = new DoubleVector(n);
+        for (int n = n1; n <= n2; n = (int) (((double) n) * Math.PI)) {
+            final Matrix matrix = generator.apply(n);
+            final DoubleVector x = rangeVector(n);
             final DoubleVector solve = new DoubleVector(conjugate.solve(matrix, matrix.multiplyBy(x).toArray()));
-            IntStream.range(0, n).forEach(i -> x.set(i, i + 1d));
             final double delta = x.subtract(solve).norm();
             final double minCond = (delta / x.norm()) / (matrix.multiplyBy(x).norm() / x.norm());
             System.out.printf("%d %d %f %f %f%n",
                     n, conjugate.getIterations(), delta, delta / x.norm(), minCond);
         }
+    }
+
+    @Test
+    public void diagonalDominanceTest() {
+        test((n) -> FormulaGenerator.generateDigMatrix(n, 0.5),
+                10, 100000);
+    }
+
+    @Test
+    public void diagonalDominanceNegativeTest() {
+        test((n) -> {
+                    final Matrix matrix = FormulaGenerator.generateDigMatrix(n, 0.5);
+                    IntStream.range(0, n).forEach(row -> {
+                        IntStream.range(0, row).forEach(col ->
+                                matrix.set(row, col, -matrix.get(row, col)));
+                        IntStream.range(row + 1, n).forEach(col ->
+                                matrix.set(row, col, -matrix.get(row, col)));
+                    });
+                    return matrix;
+                },
+                10, 100000);
+    }
+
+    @Test
+    public void hilbertTest() {
+        test(FormulaGenerator::generateHilbert, 10, 1000);
     }
 }
